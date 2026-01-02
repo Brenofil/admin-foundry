6c02837bc4b3e3e6cfec15232a51987c417a7add
Breno Filgueiras <Breno.Filgueiras@ibm.com>
2026-01-02 12:41:51 -0300

    Prettier, playwright & other configs

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import angular from 'angular-eslint';
+import sheriff from '@softarc/eslint-plugin-sheriff';
+
+export default defineConfig(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'eternal',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'eternal',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [sheriff.configs.all],
+  },
+)
